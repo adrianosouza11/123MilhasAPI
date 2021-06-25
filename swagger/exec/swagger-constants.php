35758e8b6d36bb6e97ed959969215ed94dd62b2d
Adriano Souza <adrianosouza11@gmail.com>
2021-06-25 10:03:39 -0300

    Upload params swagger-constants.php swagger-v1.php and data directory empty

---
+++ swagger/exec/swagger-constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define("API_HOST", '/123MilhasAPI/public/api/v1');
---
+++ swagger/exec/swagger-v1.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Documentação de consumo Rest API 123Milhas",
+ *     termsOfService="",
+ *    contact={
+ *     "email": "adrianosouza11@gmail.com"
+ *   }
+ * )
+ * @OA\Server(
+ *     url=API_HOST
+ * )
+ */
